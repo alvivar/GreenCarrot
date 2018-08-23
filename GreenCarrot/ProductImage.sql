489fc3146596f3c83aba03b2f10e6639e7b46cd2
alvivar <andresalvivar@gmail.com>
2018-08-23 00:35:58 -0600

    Not null

--- GreenCarrot/ProductImage.sql
+++ GreenCarrot/ProductImage.sql
@@ -3,7 +3,7 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
 	[FK_ProductImage_Product_Id] INT NOT NULL, 
     [Filepath] VARCHAR(100) NOT NULL, 
-    [Name] VARCHAR(50) NULL, 
+    [Name] VARCHAR(50) NOT NULL, 
     [Description] VARCHAR(500) NOT NULL,     
     CONSTRAINT [FK_ProductImage_Product] FOREIGN KEY ([FK_ProductImage_Product_Id]) REFERENCES [Product]([Id])
 )
