8f7af4c4a123ed7314eb1bc79606e9e354e79fa6
alvivar <andresalvivar@gmail.com>
2018-08-22 23:45:44 -0600

    The product, images and sizes

---
+++ GreenCarrot/Product.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
+    [Name] VARCHAR(50) NOT NULL, 
+    [Description] VARCHAR(50) NOT NULL, 
+    [Quantity] INT NOT NULL, 
+    [Available] BIT NOT NULL
+)
---
+++ GreenCarrot/Size.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Size]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
+    [Size] VARCHAR(50) NOT NULL 
+)
