9aa102e74add5a4c109393b097a8707296c88a42
alvivar <andresalvivar@gmail.com>
2018-08-23 02:25:23 -0600

    Provider address!

---
+++ GreenCarrot/ProviderAddress.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ProviderAddress]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
+    [FK_ProviderAddress_Provider_Id] INT NOT NULL, 
+    [FK_ProviderAddress_Address_Id] INT NOT NULL, 
+    CONSTRAINT [FK_ProviderAddress_Provider] FOREIGN KEY ([FK_ProviderAddress_Provider_Id]) REFERENCES [Provider]([Id]), 
+    CONSTRAINT [FK_ProviderAddress_Address] FOREIGN KEY ([FK_ProviderAddress_Address_Id]) REFERENCES [Address]([Id])
+)
