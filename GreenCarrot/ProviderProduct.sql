045eb56bdde502c8e276c4b0be9a0f10d21d44c5
alvivar <andresalvivar@gmail.com>
2018-08-23 00:48:27 -0600

    Provider vs Product

--- GreenCarrot/ProviderProduct.sql
+++ GreenCarrot/ProviderProduct.sql
@@ -1,4 +1,8 @@
 ﻿CREATE TABLE [dbo].[ProviderProduct]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1)
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
+    [FK_ProviderProduct_Provider_Id] INT NOT NULL, 
+    [FK_ProviderProduct_Product_Id] INT NOT NULL, 
+    CONSTRAINT [FK_ProviderProduct_Provider] FOREIGN KEY ([FK_ProviderProduct_Provider_Id]) REFERENCES [Provider]([Id]), 
+    CONSTRAINT [FK_ProviderProduct_Product] FOREIGN KEY ([FK_ProviderProduct_Product_Id]) REFERENCES [Product]([Id])
 )
