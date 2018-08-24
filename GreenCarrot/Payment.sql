bf3264d6c8d47433a5ff11f9c4a71e58dd9096be
alvivar <andresalvivar@gmail.com>
2018-08-24 04:10:32 -0600

    Payment + method

--- GreenCarrot/Payment.sql
+++ GreenCarrot/Payment.sql
@@ -2,7 +2,9 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
     [FK_Payment_Invoice_Id] INT NOT NULL, 
+    [FK_Payment_CustomerPaymentMethod_Id] INT NOT NULL, 
     [Amount] INT NOT NULL, 
     [Date] DATETIME NOT NULL, 
-    CONSTRAINT [FK_Payment_Invoice] FOREIGN KEY ([FK_Payment_Invoice_Id]) REFERENCES [Invoice]([Id])
+    CONSTRAINT [FK_Payment_Invoice] FOREIGN KEY ([FK_Payment_Invoice_Id]) REFERENCES [Invoice]([Id]), 
+    CONSTRAINT [FK_Payment_CustomerPaymentMethod] FOREIGN KEY ([FK_Payment_CustomerPaymentMethod_Id]) REFERENCES [CustomerPaymentMethod]([Id])
 )
