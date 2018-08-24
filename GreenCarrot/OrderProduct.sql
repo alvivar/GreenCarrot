a3be1a65c86d4cfeec7b4bc6d6c6b882a3dfc0ee
alvivar <andresalvivar@gmail.com>
2018-08-24 08:06:57 -0600

    Latest changes +1

--- GreenCarrot/OrderProduct.sql
+++ GreenCarrot/OrderProduct.sql
@@ -4,6 +4,7 @@
     [FK_OrderProduct_Order_Id] INT NOT NULL, 
     [FK_OrderProduct_Product_Id] INT NOT NULL, 
     [FK_OrderProduct_OrderProductStatus] INT NOT NULL, 
+    [Quantity] INT NOT NULL, 
     [Details] VARCHAR(500) NOT NULL, 
     CONSTRAINT [FK_OrderProduct_Order] FOREIGN KEY ([FK_OrderProduct_Order_Id]) REFERENCES [Order]([Id]), 
     CONSTRAINT [FK_OrderProduct_Product] FOREIGN KEY ([FK_OrderProduct_Product_Id]) REFERENCES [Product]([Id]), 
---
+++ GreenCarrot/ProviderBalance.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ProviderBalance]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+    [FK_ProviderBalance_Provider_Id] INT NOT NULL,
+    [BalanceAmount] INT NOT NULL,
+    [Details] VARCHAR(500) NOT NULL,
+    [Date] DATETIME NOT NULL,
+    CONSTRAINT [FK_ProviderBalance_Provider] FOREIGN KEY ([FK_ProviderBalance_Provider_Id]) REFERENCES [Provider]([Id])
+)
