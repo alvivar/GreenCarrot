43a95f694099a209b048f9bfae21f698e4edd3eb
alvivar <andresalvivar@gmail.com>
2018-08-24 04:11:06 -0600

    Mongo bind somehow

--- GreenCarrot/Product.sql
+++ GreenCarrot/Product.sql
@@ -1,8 +1,5 @@
 ﻿CREATE TABLE [dbo].[Product]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
-    [Name] VARCHAR(50) NOT NULL, 
-    [Description] VARCHAR(1000) NOT NULL, 
-    [Quantity] INT NOT NULL, 
-    [Available] BIT NOT NULL
+    [MongoProductName] VARCHAR(50) NOT NULL 
 )
