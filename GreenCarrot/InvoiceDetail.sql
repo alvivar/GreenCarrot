99a932da0faa0fbefa31941bab6bc51c18981875
alvivar <andresalvivar@gmail.com>
2018-08-30 23:15:38 -0600

    Invoice detail

---
+++ GreenCarrot/InvoiceDetail.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[InvoiceDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
+    [InvoiceId] INT NOT NULL, 
+    [Detail] VARCHAR(50) NOT NULL, 
+    CONSTRAINT [FK_InvoiceDetail_Invoice] FOREIGN KEY ([InvoiceId]) REFERENCES [Invoice]([Id])
+)
