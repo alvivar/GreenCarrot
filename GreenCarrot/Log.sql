16b97171e9d6008ff3308c2376e9283a021b758d
alvivar <andresalvivar@gmail.com>
2018-09-02 16:51:58 -0600

    Bitácora

---
+++ GreenCarrot/Log.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[Log]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+    [LogLeveId] INT NOT NULL,
+    [LogTypeId] INT NOT NULL,
+    [LogSourceId] INT NOT NULL,
+    [UserId] INT NOT NULL,
+    [UserName] VARCHAR(50) NOT NULL,
+    [Computer] VARCHAR(50) NOT NULL,
+    [Ip] VARCHAR(50) NOT NULL,
+    [Description] VARCHAR(1000) NOT NULL,
+    [Details] VARCHAR(1000) NOT NULL,
+    [ReferenceId1] INT NULL,
+    [ReferenceId2] INT NULL,
+    CONSTRAINT [FK_Log_LogLevel] FOREIGN KEY (LogLeveId) REFERENCES [LogLevel]([Id]),
+    CONSTRAINT [FK_Log_LogType] FOREIGN KEY (LogTypeId) REFERENCES [LogType]([Id]),
+    CONSTRAINT [FK_Log_LogSource] FOREIGN KEY (LogSourceId) REFERENCES [LogSource]([Id]),
+    CONSTRAINT [FK_Log_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id])
+)
---
+++ GreenCarrot/LogSource.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[LogSource]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+    [Name] VARCHAR(50) NOT NULL,
+    [Description] VARCHAR(500) NOT NULL
+)
