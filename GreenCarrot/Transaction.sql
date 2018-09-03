f5aecda79301022ec2c57711fa49bfb7c30806c3
alvivar <andresalvivar@gmail.com>
2018-09-02 21:06:24 -0600

    Transaction #wip

---
+++ GreenCarrot/Transaction.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Transaction]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	[TransactionDetailId] INT NOT NULL,
+	CONSTRAINT [FK_Transaction_TransactionDetail] FOREIGN KEY ([TransactionDetailId]) REFERENCES [TransactionDetail]([Id])
+)
