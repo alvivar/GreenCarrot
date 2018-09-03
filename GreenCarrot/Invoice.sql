83d09d76c14a1783c55e7cca70a92a5fbd81a469
alvivar <andresalvivar@gmail.com>
2018-09-02 20:28:12 -0600

    Invoices are generated after payment

--- GreenCarrot/Invoice.sql
+++ GreenCarrot/Invoice.sql
@@ -1,9 +1,10 @@
 ﻿CREATE TABLE [dbo].[Invoice]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
-    [CassandraOrderId] VARCHAR(50) NOT NULL, 
-    [InvoiceStatusId] INT NOT NULL, 
-    [Description] VARCHAR(500) NOT NULL, 
-    [Date] DATETIME NOT NULL, 
-    CONSTRAINT [FK_Invoice_InvoiceStatus] FOREIGN KEY ([InvoiceStatusId]) REFERENCES [InvoiceStatus]([Id]) 
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+    [PaymentId] INT NOT NULL,
+    [InvoiceStatusId] INT NOT NULL,
+    [Description] VARCHAR(500) NOT NULL,
+    [CreatedDate] DATETIME NOT NULL,
+    CONSTRAINT [FK_Invoice_Payment] FOREIGN KEY ([PaymentId]) REFERENCES [Payment]([Id]),
+    CONSTRAINT [FK_Invoice_InvoiceStatus] FOREIGN KEY ([InvoiceStatusId]) REFERENCES [InvoiceStatus]([Id])
 )
--- GreenCarrot/Payment.sql
+++ GreenCarrot/Payment.sql
@@ -1,10 +1,13 @@
 ﻿CREATE TABLE [dbo].[Payment]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
-    [InvoiceId] INT NOT NULL, 
-    [UserPaymentMethodId] INT NOT NULL, 
-    [Amount] INT NOT NULL, 
-    [Date] DATETIME NOT NULL, 
-    CONSTRAINT [FK_Payment_Invoice] FOREIGN KEY ([InvoiceId]) REFERENCES [Invoice]([Id]), 
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+    [CassandraOrderId] VARCHAR(50) NOT NULL,
+    [UserPaymentMethodId] INT NOT NULL,
+    [Amount] DECIMAL(8, 3) NOT NULL,
+    [TaxAmout] DECIMAL(8, 3) NOT NULL,
+    [TaxPercentage] DECIMAL(4, 3) NOT NULL,
+    [TaxZipCode] VARCHAR(50) NOT NULL,
+    [PaidDate] DATETIME NOT NULL,
+    [CheckSum] VARCHAR(50) NOT NULL,
     CONSTRAINT [FK_Payment_UserPaymentMethod] FOREIGN KEY ([UserPaymentMethodId]) REFERENCES [UserPaymentMethod]([Id])
 )
