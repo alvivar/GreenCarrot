01649b4267e7042057cd0421d10f11dba34ff3fe
alvivar <andresalvivar@gmail.com>
2018-09-03 07:46:43 -0600

    Transactions v1

--- GreenCarrot/Transaction.sql
+++ GreenCarrot/Transaction.sql
@@ -1,6 +1,12 @@
 ﻿CREATE TABLE [dbo].[Transaction]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+    [TransactionStatusId] INT NOT NULL, 
+	[TransactionTypeId] INT NOT NULL, 
 	[TransactionDetailId] INT NOT NULL,
-	CONSTRAINT [FK_Transaction_TransactionDetail] FOREIGN KEY ([TransactionDetailId]) REFERENCES [TransactionDetail]([Id])
+    [Amount] DECIMAL(8, 4) NOT NULL, 
+    [Created] DATETIME NOT NULL, 
+    CONSTRAINT [FK_Transaction_Status] FOREIGN KEY ([TransactionStatusId]) REFERENCES [TransactionStatus]([Id]),
+    CONSTRAINT [FK_Transaction_TransactionType] FOREIGN KEY ([TransactionTypeId]) REFERENCES [TransactionType]([Id]),
+    CONSTRAINT [FK_Transaction_TransactionDetail] FOREIGN KEY ([TransactionDetailId]) REFERENCES [TransactionDetail]([Id])
 )
--- GreenCarrot/TransactionDetail.sql
+++ GreenCarrot/TransactionDetail.sql
@@ -1,4 +1,8 @@
 ﻿CREATE TABLE [dbo].[TransactionDetail]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1)
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	[DebitedUserId] INT NOT NULL,
+	[CreditedUserId] INT NOT NULL,
+	CONSTRAINT [FK_TransactionDetail_User1] FOREIGN KEY ([DebitedUserId]) REFERENCES [User]([Id]),
+	CONSTRAINT [FK_TransactionDetail_User2] FOREIGN KEY ([CreditedUserId]) REFERENCES [User]([Id])
 )
