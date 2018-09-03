feab1efbbfc05808b3535fee56aef5c1b9fc4b76
alvivar <andresalvivar@gmail.com>
2018-09-02 21:04:14 -0600

    Balance deserve the name

---
+++ GreenCarrot/Balance.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Balance]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
+    [UserId] INT NOT NULL, 
+    [Amount] INT NOT NULL, 
+    [Details] VARCHAR(500) NOT NULL, 
+    [Transacted] DATETIME NOT NULL, 
+    CONSTRAINT [FK_Balance_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id])
+)
--- GreenCarrot/UserBalance.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[UserBalance]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
-    [UserId] INT NOT NULL, 
-    [BalanceAmount] INT NOT NULL, 
-    [Details] VARCHAR(500) NOT NULL, 
-    [Date] DATETIME NOT NULL, 
-    CONSTRAINT [FK_UserBalance_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id])
-)
