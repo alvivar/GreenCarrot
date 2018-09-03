4500c39886cd22bf797513892613a7d3e86195a7
alvivar <andresalvivar@gmail.com>
2018-09-02 20:27:48 -0600

    User priority

--- GreenCarrot/Log.sql
+++ GreenCarrot/Log.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[Log]
 (
     [Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+    [UserId] INT NOT NULL,
     [LogLeveId] INT NOT NULL,
     [LogTypeId] INT NOT NULL,
     [LogSourceId] INT NOT NULL,
-    [UserId] INT NOT NULL,
     [UserName] VARCHAR(50) NOT NULL,
     [Computer] VARCHAR(50) NOT NULL,
     [Ip] VARCHAR(50) NOT NULL,
@@ -12,8 +12,10 @@
     [Details] VARCHAR(1000) NOT NULL,
     [ReferenceId1] INT NULL,
     [ReferenceId2] INT NULL,
+	[CheckSum] VARCHAR(50) NOT NULL,
+    [LoggedDate] DATETIME NOT NULL,
+    CONSTRAINT [FK_Log_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id]),
     CONSTRAINT [FK_Log_LogLevel] FOREIGN KEY (LogLeveId) REFERENCES [LogLevel]([Id]),
     CONSTRAINT [FK_Log_LogType] FOREIGN KEY (LogTypeId) REFERENCES [LogType]([Id]),
-    CONSTRAINT [FK_Log_LogSource] FOREIGN KEY (LogSourceId) REFERENCES [LogSource]([Id]),
-    CONSTRAINT [FK_Log_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id])
+    CONSTRAINT [FK_Log_LogSource] FOREIGN KEY (LogSourceId) REFERENCES [LogSource]([Id])
 )
