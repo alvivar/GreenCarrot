94f871e43528d58105ae2f4b2e2b251b1e17e206
alvivar <andresalvivar@gmail.com>
2018-09-03 07:41:09 -0600

    User + Auth

--- GreenCarrot/Authentication.sql
+++ GreenCarrot/Authentication.sql
@@ -1,4 +1,14 @@
 ﻿CREATE TABLE [dbo].[Authentication]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1)
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	[AuthenticationSourceId] INT NOT NULL,
+    [ClientId] VARCHAR(50) NOT NULL,
+    [ClientSecret] VARCHAR(50) NOT NULL,
+    [RedirectUri] VARCHAR(50) NOT NULL,
+    [Scope] VARCHAR(50) NOT NULL,
+    [ExpiresIn] INT NOT NULL,
+    [Checksum] VARCHAR(50) NOT NULL,
+    [Created] DATETIME NOT NULL,
+    CONSTRAINT [FK_Authentication_AuthenticationSource] FOREIGN KEY ([AuthenticationSourceId]) REFERENCES [AuthenticationSource]([Id])
+
 )
---
+++ GreenCarrot/UserAuthentication.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserAuthentication]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	[UserId] INT NOT NULL,
+	[AuthenticationId] INT NOT NULL,
+	CONSTRAINT [FK_UserAuthentication_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id]),
+	CONSTRAINT [FK_UserAuthentication_Authentication] FOREIGN KEY ([AuthenticationId]) REFERENCES [Authentication]([Id])
+)
