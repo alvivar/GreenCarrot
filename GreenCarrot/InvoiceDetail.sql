0dae7bc0a740849e7af28ee39fb996c73cc123d7
alvivar <andresalvivar@gmail.com>
2018-09-02 21:03:10 -0600

    Product bind

--- GreenCarrot/InvoiceDetail.sql
+++ GreenCarrot/InvoiceDetail.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[InvoiceDetail]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
-    [InvoiceId] INT NOT NULL, 
-    [Detail] VARCHAR(50) NOT NULL, 
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+    [InvoiceId] INT NOT NULL,
+    [CassandraProductId] VARCHAR(50) NOT NULL,
+    [Description] VARCHAR(500) NOT NULL,
     CONSTRAINT [FK_InvoiceDetail_Invoice] FOREIGN KEY ([InvoiceId]) REFERENCES [Invoice]([Id])
 )
