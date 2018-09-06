d065d42fb9e4167eb0121c2afc7f2d5c6d42ee5e
alvivar <andresalvivar@gmail.com>
2018-09-06 01:16:56 -0600

    Transaction subtype

--- GreenCarrot/Transaction.sql
+++ GreenCarrot/Transaction.sql
@@ -1,12 +1,15 @@
 ﻿CREATE TABLE [dbo].[Transaction]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
-    [TransactionStatusId] INT NOT NULL, 
-	[TransactionTypeId] INT NOT NULL, 
-	[TransactionDetailId] INT NOT NULL,
-    [Amount] DECIMAL(19, 4) NOT NULL, 
-    [Created] DATETIME NOT NULL, 
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+    [TransactionStatusId] INT NOT NULL,
+    [TransactionTypeId] INT NOT NULL,
+    [TransactionSubtype] INT NOT NULL,
+    [TransactionDetailId] INT NOT NULL,
+    [Amount] DECIMAL(19, 4) NOT NULL,
+    [Description] VARCHAR(500) NOT NULL,
+    [Created] DATETIME NOT NULL,
     CONSTRAINT [FK_Transaction_Status] FOREIGN KEY ([TransactionStatusId]) REFERENCES [TransactionStatus]([Id]),
     CONSTRAINT [FK_Transaction_TransactionType] FOREIGN KEY ([TransactionTypeId]) REFERENCES [TransactionType]([Id]),
+    CONSTRAINT [FK_Transaction_TransactionSubtype] FOREIGN KEY ([Column]) REFERENCES [ToTable]([ToTableColumn]),
     CONSTRAINT [FK_Transaction_TransactionDetail] FOREIGN KEY ([TransactionDetailId]) REFERENCES [TransactionDetail]([Id])
 )
