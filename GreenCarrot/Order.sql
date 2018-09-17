64b98bfb4b675012dd1c67cd066c3625cd9783e2
alvivar <andresalvivar@gmail.com>
2018-09-16 18:19:07 -0600

    datetime

--- GreenCarrot/Order.sql
+++ GreenCarrot/Order.sql
@@ -4,6 +4,7 @@
 	[UserId] INT NOT NULL,
 	[OrderStatusId] INT NOT NULL,
 	[Description] VARCHAR(500) NOT NULL,
-	CONSTRAINT [FK_Order_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id]),
+	[Created] DATETIME NOT NULL, 
+    CONSTRAINT [FK_Order_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id]),
 	CONSTRAINT [FK_Order_OrderStatus] FOREIGN KEY ([OrderStatusId]) REFERENCES [OrderStatus]([Id])
 )
