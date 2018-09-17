eddb441fd14f6da5ce4adc9a2914017cda46e611
alvivar <andresalvivar@gmail.com>
2018-09-16 18:33:27 -0600

    Audit info

--- GreenCarrot/Balance.sql
+++ GreenCarrot/Balance.sql
@@ -3,9 +3,13 @@
     [Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
     [UserId] INT NOT NULL,
     [TransactionId] INT NOT NULL,
+    [InsertionUserId] INT NOT NULL,
+    [Computer] VARCHAR(100) NOT NULL,
     [Amount] DECIMAL(19, 4) NOT NULL,
     [Description] VARCHAR(500) NOT NULL,
     [Created] DATETIME NOT NULL,
+    [CheckSum] VARBINARY(100) NOT NULL,
     CONSTRAINT [FK_Balance_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id]),
-    CONSTRAINT [FK_Balance_Transaction] FOREIGN KEY ([TransactionId]) REFERENCES [Transaction]([Id])
+    CONSTRAINT [FK_Balance_Transaction] FOREIGN KEY ([TransactionId]) REFERENCES [Transaction]([Id]),
+    CONSTRAINT [FK_Balance_InsertionUser] FOREIGN KEY ([InsertionUserId]) REFERENCES [User]([Id])
 )
