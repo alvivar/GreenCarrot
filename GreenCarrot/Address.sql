194e9d1390ab92411b61a826ec255c066ad70865
alvivar <andresalvivar@gmail.com>
2018-09-16 18:32:36 -0600

    Data improved

--- GreenCarrot/Address.sql
+++ GreenCarrot/Address.sql
@@ -1,15 +1,16 @@
 ﻿CREATE TABLE [dbo].[Address]
 (
     [Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
-    [Line1] VARCHAR(250) NOT NULL,
-    [Line2] VARCHAR(250) NOT NULL,
-    [Line3] VARCHAR(250) NOT NULL,
+    [Line1] VARCHAR(500) NOT NULL,
+    [Line2] VARCHAR(500) NOT NULL,
+    [Line3] VARCHAR(500) NOT NULL,
     [CityId] INT NOT NULL,
     [ProvinceId] INT NOT NULL,
     [CountryId] INT NOT NULL,
     [ZipCode] VARCHAR(10) NOT NULL,
-    [Details] VARCHAR(1000) NOT NULL,
+    [Description] VARCHAR(1000) NOT NULL,
     [Geography] [sys].[geography] NOT NULL,
+    [Created] DATETIME NOT NULL,
     CONSTRAINT [FK_Address_City] FOREIGN KEY ([CityId]) REFERENCES [City]([Id]),
     CONSTRAINT [FK_Address_Province] FOREIGN KEY ([ProvinceId]) REFERENCES [Province]([Id]),
     CONSTRAINT [FK_Address_Country] FOREIGN KEY ([CountryId]) REFERENCES [Country]([Id])
