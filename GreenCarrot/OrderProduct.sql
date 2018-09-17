53c38aee0b413ecbea8949af8f89f007795a9ed7
alvivar <andresalvivar@gmail.com>
2018-09-16 18:16:17 -0600

    Quantity

--- GreenCarrot/OrderProduct.sql
+++ GreenCarrot/OrderProduct.sql
@@ -4,6 +4,7 @@
     [OrderId] INT NOT NULL, 
 	[ProductId] INT NOT NULL, 
 	[OrderProductStatusId] INT NOT NULL, 
+    [Quantity] INT NOT NULL, 
     CONSTRAINT [FK_OrderProduct_Order] FOREIGN KEY ([OrderId]) REFERENCES [Order]([Id]), 
     CONSTRAINT [FK_OrderProduct_Product] FOREIGN KEY ([ProductId]) REFERENCES [Product]([Id]),
     CONSTRAINT [FK_OrderProduct_OrderProductStatus] FOREIGN KEY ([OrderProductStatusId]) REFERENCES [OrderProductStatus]([Id])
