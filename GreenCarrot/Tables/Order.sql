367ac09b8c9150ad5c0f4e3e25badfda335dc0b4
Andrés Villalobos <andres.villalobos@sweetrush.com>
2018-09-20 11:05:54 -0600

    They can be null because of life

--- GreenCarrot/Tables/Order.sql
+++ GreenCarrot/Tables/Order.sql
@@ -2,7 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
 	[UserId] INT NOT NULL,
-	[OrderStatusId] INT NOT NULL,
+	[OrderStatusId] INT NULL,
 	[Description] VARCHAR(500) NOT NULL,
 	[Created] DATETIME NOT NULL, 
     CONSTRAINT [FK_Order_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id]),
