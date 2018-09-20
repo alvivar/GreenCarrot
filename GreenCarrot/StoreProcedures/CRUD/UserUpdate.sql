c5c6fa554c39114c3a44bc5e3ddf5d2f64a03c27
Andrés Villalobos <andres.villalobos@sweetrush.com>
2018-09-20 00:26:11 -0600

    User update

---
+++ GreenCarrot/StoreProcedures/CRUD/UserUpdate.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[UserUpdate]
+	@UserName VARCHAR(100)
+	,@Name VARCHAR(100)
+	,@LastName VARCHAR(100)
+	,@Email VARCHAR(100)
+AS
+SET NOCOUNT ON
+SET XACT_ABORT ON
+
+BEGIN TRAN
+
+-- Update
+UPDATE [dbo].[User]
+SET [Name] = @Name, [LastName] = @LastName, [Email] = @Email
+WHERE [UserName] = @UserName
+
+-- Updated Id
+DECLARE @UpdatedId INT
+SELECT @UpdatedId = [Id]
+FROM [dbo].[User]
+WHERE [UserName] = @UserName
+
+COMMIT
+
+-- Return
+IF @UpdatedId IS NULL
+RETURN -1		-- It means error
+ELSE
+RETURN @UpdatedId
+
+GO
