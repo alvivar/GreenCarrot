4a700799fa65b3296b05411e3242d2269d87850f
Andrés Villalobos <andres.villalobos@sweetrush.com>
2018-09-20 12:36:37 -0600

    Inserts!

---
+++ GreenCarrot/BalanceInsert.sql
@@ -0,0 +1,48 @@
+﻿CREATE PROCEDURE [dbo].[BalanceInsert]
+	@UserId INT
+	,@TransactionId INT
+	,@InsertionUserId INT
+	,@Computer VARCHAR(100)
+	,@Amount DECIMAL(19, 4)
+	,@Description VARCHAR(500)
+	,@Created DATETIME
+	,@CheckSum VARBINARY(100)
+AS
+SET NOCOUNT ON
+SET XACT_ABORT ON
+
+BEGIN TRAN
+
+INSERT INTO [dbo].[Balance]
+	([UserId]
+	,[TransactionId]
+	,[InsertionUserId]
+	,[Computer]
+	,[Amount]
+	,[Description]
+	,[Created]
+	,[CheckSum])
+SELECT @UserId
+	,@TransactionId
+	,@InsertionUserId
+	,@Computer
+	,@Amount
+	,@Description
+	,@Created
+	,@CheckSum
+
+SELECT [Id]
+	,[UserId]
+	,[TransactionId]
+	,[InsertionUserId]
+	,[Computer]
+	,[Amount]
+	,[Description]
+	,[Created]
+	,[CheckSum]
+FROM [dbo].[Balance]
+WHERE  [Id] = SCOPE_IDENTITY()
+
+COMMIT
+RETURN SCOPE_IDENTITY()
+GO
---
+++ GreenCarrot/TransactionInsert.sql
@@ -0,0 +1,47 @@
+﻿CREATE PROCEDURE [dbo].[TransactionInsert]
+	@TransactionStatusId INT
+	,@TransactionTypeId INT
+	,@TransactionSubtypeId INT
+	,@TransactionDetailId INT
+	,@InsertionUserId INT
+	,@Computer VARCHAR(100)
+	,@Amount DECIMAL(19, 4)
+	,@Description VARCHAR(500)
+	,@Created DATETIME
+	,@Checksum VARBINARY(100)
+AS
+SET NOCOUNT ON
+SET XACT_ABORT ON
+
+BEGIN TRAN
+
+INSERT INTO [dbo].[Transaction]
+	([TransactionStatusId], [TransactionTypeId], [TransactionSubtypeId], [TransactionDetailId], [InsertionUserId], [Computer], [Amount], [Description], [Created], [Checksum])
+SELECT @TransactionStatusId
+	,@TransactionTypeId
+	,@TransactionSubtypeId
+	,@TransactionDetailId
+	,@InsertionUserId
+	,@Computer
+	,@Amount
+	,@Description
+	,@Created
+	,@Checksum
+
+SELECT [Id]
+	,[TransactionStatusId]
+	,[TransactionTypeId]
+	,[TransactionSubtypeId]
+	,[TransactionDetailId]
+	,[InsertionUserId]
+	,[Computer]
+	,[Amount]
+	,[Description]
+	,[Created]
+	,[Checksum]
+FROM [dbo].[Transaction]
+WHERE  [Id] = SCOPE_IDENTITY()
+
+COMMIT
+RETURN  SCOPE_IDENTITY()
+GO
